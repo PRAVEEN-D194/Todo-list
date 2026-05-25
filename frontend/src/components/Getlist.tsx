e61b430b1d4d22964e634fadbba132cc3d31f4aa
PRAVEEN-D194 <praveend12321@gmail.com>
2026-05-25 18:50:02 +0530

    css change

--- frontend/src/components/Getlist.tsx
+++ frontend/src/components/Getlist.tsx
@@ -25,7 +25,7 @@ export default function Getlsit({item, index}:any) {
     }
 
     return (<>
-     <div className="flex bg-blue-300 w-full max-w-[1200px] mx-auto py-6 sm:py-10 rounded-lg justify-around items-center px-4">
+    <div className="flex bg-blue-300 w-full max-w-[1200px] mx-auto py-2 sm:py-10 rounded-lg justify-between items-start px-2 gap-4">
 
     {/* LEFT */}
     <div className="flex flex-col gap-2">
@@ -42,24 +42,24 @@ export default function Getlsit({item, index}:any) {
     </div>
 
     {/* MIDDLE */}
-    <div className="flex-1 px-2 sm:px-4">
-        <h1 className={completed ? "line-through text-gray-500 text-base sm:text-2xl " : "font-bold text-base sm:text-2xl text-green-700"}>
+    <div className="flex-1 px-2 sm:px-4 break-words overflow-hidden">
+        <h1 className={`${completed ? "line-through text-gray-500 text-base sm:text-2xl " : "font-bold text-base sm:text-2xl text-green-700"}text-base sm:text-2xl break-words`}>
             {item.name}
         </h1>
-        <h1 className={completed? " line-through text-gray-500 text-sm sm:text-2xl" : "text-sm sm:text-2xl text-gray-800"}>
+        <h1 className={`${completed? " line-through text-gray-500 text-sm sm:text-2xl" : "text-sm sm:text-2xl text-gray-800"} text-sm sm:text-2xl break-words`}>
             {item.description}
         </h1>
     </div>
 
     {/* RIGHT */}
-    <div className="flex flex-col sm:flex-row gap-2 sm:gap-3 w-[140px] sm:w-[200px] justify-end mr-5 sm:mr-5">
+    <div className="flex flex-col sm:flex-row gap-2 sm:gap-3 justify-end">
 
-        <button onClick={ondelete} className="bg-red-700 text-white text-xs sm:text-sm px-3 sm:px-5 py-2 sm:py-4 rounded-4xl hover:bg-red-400 active:bg-red-900">
-            delete
+        <button onClick={ondelete} className="bg-red-700 text-white text-xs sm:text-sm px-2 sm:px-5 py-1 sm:py-4 rounded-2xl hover:bg-red-400 active:bg-red-900">
+            Delete
         </button>
 
-        <button onClick={()=>{setupdate(true)} } className="bg-blue-700 text-white text-xs sm:text-sm px-3 sm:px-5 py-2 sm:py-4 rounded-4xl hover:bg-blue-400 active:bg-blue-900">
-            update
+        <button onClick={()=>{setupdate(true)} } className="bg-blue-700 text-white text-xs sm:text-sm px-2 sm:px-5 py-1 sm:py-4 rounded-2xl hover:bg-blue-400 active:bg-blue-900">
+            Update
         </button>
         <Updatelist upopen={upopen} setupdate={setupdate} item={item}></Updatelist>
 
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -22,11 +22,11 @@ export default function Home(){
     return(
         <>
         <div className=" flex flex-col items-center  mt-10">
-            <h1 className="font-bold text-5xl text-red-500 ">To Do List</h1>
+            <h1 className="font-bold text-3xl sm:text-5xl text-red-500 text-center">To Do List</h1>
 
-            <button  className=" font-bold text-5xl  text-gree-500 px-20 py-10 bg-violet-200 rounded-4xl mt-10 mb-10 sm:px10 sm:py-5" onClick={()=>{setlistopen(true)}}>Add list</button>
+            <button  className="font-bold text-xl sm:text-3xl px-6 py-3 sm:px-16 sm:py-6 bg-violet-200 rounded-3xl mt-6 mb-6 hover:bg-violet-300 active:bg-violet-400 transition" onClick={()=>{setlistopen(true)}}>Add list</button>
             <Postlist listopen={listopen} setlistopen={setlistopen}></Postlist>
-            <div className="flex flex-col gap-4 place-items-center ">
+            <div className="flex flex-col sm:flex-row bg-blue-300 sm:w-[65%] w-[95%] max-w-[1200px] mx-auto p-2 sm:p-2 rounded-lg gap-4">
             {list.map((list, index)=>(
                 <Getlsit
                     key={index}
